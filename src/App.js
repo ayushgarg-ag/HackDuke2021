c8e21474db7e6a191eab16483a3c41f7b7f06f06
Vaibhav Sharma <vaibhav.sharma@duke.edu>
2021-10-23 16:53:23 -0400

    react axios post and get

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+
 
 function App() {
+  const [getData, setData] = useState({})
+
+  // useEffect(()=>{
+  //   axios.get("/members").then(
+  //     response => response
+  //     ).then(
+  //       data => {
+  //         setData(data)
+  //         console.log(data)
+  //       }
+  //     )
+  // }, [])
+  useEffect(()=> {
+    axios({
+      method: 'post',
+      url: '/url',
+      data: {
+        url: 'https://s3-us-west-2.amazonaws.com/blog.assemblyai.com/audio/8-7-2018-post/7510.mp3'
+      }
+    })
+  }, [])
+  // useEffect(()=> {
+  //   axios.post('/url', {
+  //     url: 'https://s3-us-west-2.amazonaws.com/blog.assemblyai.com/audio/8-7-2018-post/7510.mp3'
+  //   })
+  //   .then(function (response) {
+  //     console.log(response);
+  //   })
+  //   .catch(function (error) {
+  //     console.log(error);
+  //   });
+
+  // }, [])
+
+  useEffect(()=>{
+    axios.get('/url').then(response => {
+      console.log("SUCCESS", response)
+      setData(response.data)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>React + Flask Tutorial</p>
       </header>
+    {(typeof getData.members=='undefined') ? (<p>Loading</p>) : (getData.members.map((member,i) => (<p key={i}>{member}</p>)))}
     </div>
   );
 }
