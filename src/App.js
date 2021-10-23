d6e35974e2600bac039a1837cbb1a22f069d066b
Vaibhav Sharma <vaibhav.sharma@duke.edu>
2021-10-23 17:44:27 -0400

    Pushing working text display

--- src/App.js
+++ src/App.js
@@ -1,62 +1,176 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useEffect, useState } from 'react';
-import axios from 'axios'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const Upload = (props) => {
+    const [file, setFile] = useState("");
+    const [fileSubmitted, setFileSubmitted] = useState(false);
+    const [transcription, setTranscription] = useState("");
 
 
-function App() {
-  const [getData, setData] = useState({})
+    const fileURLHandler = (event) => {
+        event.preventDefault()
+        console.log(event.target.value)
+        setFile(event.target.value)
+        setFileSubmitted(false);
+    }
 
-  // useEffect(()=>{
-  //   axios.get("/members").then(
-  //     response => response
-  //     ).then(
-  //       data => {
-  //         setData(data)
-  //         console.log(data)
-  //       }
-  //     )
-  // }, [])
-  useEffect(()=> {
-    axios({
-      method: 'post',
-      url: '/url',
-      data: {
-        url: 'https://s3-us-west-2.amazonaws.com/blog.assemblyai.com/audio/8-7-2018-post/7510.mp3'
-      }
-    })
-  }, [])
-  // useEffect(()=> {
-  //   axios.post('/url', {
-  //     url: 'https://s3-us-west-2.amazonaws.com/blog.assemblyai.com/audio/8-7-2018-post/7510.mp3'
-  //   })
-  //   .then(function (response) {
-  //     console.log(response);
-  //   })
-  //   .catch(function (error) {
-  //     console.log(error);
-  //   });
+    const submitHandler = (event) => {
+        event.preventDefault()
+        console.log("button pressed!")
+        console.log(file)
+        setFileSubmitted(true);
 
-  // }, [])
+        axios.post('/url', {
+          url: file
+        })
+        .then(function (response)  {
+          console.log(response.data);
+          setTranscription(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
 
-  useEffect(()=>{
-    axios.get('/url').then(response => {
-      console.log("SUCCESS", response)
-      setData(response.data)
-    }).catch(error => {
-      console.log(error)
-    })
+      return (
+          <p>{file}</p>
+      )
+    }
 
-  }, [])
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>React + Flask Tutorial</p>
-      </header>
-    {(typeof getData.members=='undefined') ? (<p>Loading</p>) : (getData.members.map((member,i) => (<p key={i}>{member}</p>)))}
-    </div>
-  );
+    const displayTrans = () => {
+        return (
+            <p>{transcription}</p>
+        )
+    }
+
+    return (
+        <div class="upload">
+            <h3>Paste your audio file link here.</h3>
+            <form>
+              <input type="text" onChange={fileURLHandler}/>
+              <button type="submit" onClick={submitHandler}>Upload</button>
+            </form>
+            {fileSubmitted ? displayTrans() : ""}
+        </div>
+    )
+}
+
+const Why = (props) => {
+    return (
+        <div class="why">
+            <h2>Why LectureNotes?</h2>
+
+            <p>As schools adjust to new pandemic guidelines,
+            learning formats are up-in-the-air.</p>
+
+            <p>
+                Some classes are held in-person, which leaves
+                little room for those attending remotely
+                or who are immunocompromised.
+            </p>
+
+            <p>
+                For those folks, if the proper virtual-learning technology
+                is not properly in-place in the classroom,
+                there are many barriers to attending class.
+            </p>
+
+            <p>
+                LectureNotes makes it easy for both the lecturer and the
+                hybrid student
+            </p>
+
+            <ul>
+
+            </ul>
+        </div>
+    )
+}
+
+const How = (props) => {
+    return (
+        <div class="how">
+            <h2>3 Simple Steps</h2>
+
+            <ol>
+                <li><strong>Record.</strong> The lecturer records their audio during the class — this requires no complicated virtual-learning technical setup. </li>
+                <li><strong>Upload</strong> the audio file to LectureNotes.</li>
+                <li><strong>Notes.</strong> The hybrid student can now stay up-to-date with a live transcript of the class.</li>
+            </ol>
+        </div>
+    )
+}
+
+const Scroll = (props) => {
+    return (
+
+        <div class="mouse_scroll">
+
+    		<div>
+    			<span class="m_scroll_arrows unu"></span>
+    			<span class="m_scroll_arrows doi"></span>
+    			<span class="m_scroll_arrows trei"></span>
+    		</div>
+        </div>
+    )
+}
+
+const Team = (props) => {
+    return (
+        <div class="team">
+            <p>
+                Built @ HackDuke:
+                    Ayush / Aditya / Prajwal / Bob / Vaibhov
+            </p>
+        </div>
+    )
+}
+
+const App = (props) => {
+    const [notes, setNotes] = useState(props.notes);
+    const [newNote, setNewNote] = useState("a new note...");
+
+    const addNote = (event) => {
+        event.preventDefault();
+        console.log("button clicked", event.target);
+
+        const noteObject = {
+            id: notes.length + 1,
+            content: newNote,
+            date: new Date().toISOString(),
+            important: Math.random() < 0.5
+        }
+
+        setNotes(notes.concat(noteObject));
+        setNewNote("");
+    }
+
+    const handleNoteChange = (event) => {
+        console.log(event.target.value);
+        setNewNote(event.target.value);
+    }
+
+    return (
+        <div>
+            <div class="title">
+                <div class="logo">
+                    <p>LectureNotes</p>
+                </div>
+                <div class="desc">
+                    <h1>Transcribe Your Lecture</h1>
+                    <p>With LectureNotes, an audio-to-text tool</p>
+                    <Scroll />
+                </div>
+            </div>
+            <Why />
+            <How />
+            <Upload />
+            <Team />
+
+            <p></p>
+        </div>
+    );
 }
 
 export default App;
